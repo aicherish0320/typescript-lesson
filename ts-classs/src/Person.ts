5c184c22070b1f578c5bf10325b6278deada5f64
aicherish <aicherish0320@163.com>
2022-09-22 21:06:12 +0800

    ts 类的基本概念

--- ts-classs/src/Person.ts
+++ ts-classs/src/Person.ts
@@ -6,11 +6,20 @@ class Person {
   public doEat(who: string, address: string): void {
     console.log(`${this.name}和${who}在${address}吃饭`)
   }
+
+  constructor(name_: string, age_: number, phone_: string) {
+    this.name = name_
+    this.age = age_
+    this.phone = phone_
+  }
 }
 
-const p1 = new Person()
-p1.name = 'aic'
-p1.age = 25
-p1.phone = '12345678'
+const p1 = new Person('jack', 25, '12345')
+// p1.name = 'aic'
+// p1.age = 25
+// p1.phone = '12345678'
 
-p1.doEat('jack', '牛家屯')
+p1.doEat('rose', '牛家屯')
+
+console.log(p1)
+console.log(Person.prototype.constructor === Person)
